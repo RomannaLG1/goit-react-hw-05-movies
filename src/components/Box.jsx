948a03ba700021ef6577dd0dba443f7d22b2d7f7
RomannaLG1 <melnykromasja@gmail.com>
2022-12-23 10:54:03 +0200

    add start files

---
+++ src/components/Box.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+import { color, space, layout, border, flexbox, grid } from 'styled-system'
+
+const Box = styled('div')(color, border, space, layout, flexbox, grid);
+
+
+export default Box;
